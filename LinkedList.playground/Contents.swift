7e768ce765a90fb72a81868412b9e05a9ee41523
Preetam Jadakar <preetamjadakar@gmail.com>
2020-03-28 21:43:41 +0530

    feat: pop, removeLast, remove at given index, append, insert at given index

--- LinkedList.playground/Contents.swift
+++ LinkedList.playground/Contents.swift
@@ -17,6 +17,98 @@ struct LinkedList<Value> {
             head = Node.init(value: value, next: head)
         }
     }
+    
+    mutating func pop() -> Node<Value>? {
+        guard head != nil else {
+            print("popping can't be done on empty list")
+            return nil
+        }
+        let deletedNode = head
+        head = head?.next
+        return deletedNode
+    }
+    
+    mutating func removeLast() -> Value? {
+        guard head != nil else {
+            print("can't perform `removeLast` for empty list")
+            return nil
+        }
+        guard head?.next != nil else {
+            return pop()?.value
+        }
+        var iterator = head
+        
+        while iterator?.next?.next != nil { // previous of tail
+            iterator = iterator?.next
+        }
+        let currentTail = tail
+        tail = iterator
+        tail?.next = nil
+        return currentTail?.value
+    }
+    
+    mutating func remove(at index:Int) -> Value? {
+        guard !isEmpty else {
+            print("list is empty")
+            return nil
+        }
+        guard head !== tail else {
+            return pop()?.value
+        }
+        if index == 0 {
+            defer {
+                head = head?.next
+            }
+            return head?.value
+        } else {
+        var targetNode = head
+        var iterator = 0
+        while head?.next != nil,  iterator < index - 1 {
+            targetNode = targetNode?.next
+            iterator += 1
+        }
+        //remove node
+        defer {
+            targetNode?.next = targetNode?.next?.next
+        }
+        return targetNode?.next?.value
+        }
+    }
+    
+    mutating func append(value: Value) {
+        if isEmpty {
+            head = Node.init(value: value)
+            tail = head
+        } else {
+            let newNode = Node.init(value: value)
+            tail?.next = newNode
+            tail = newNode
+        }
+    }
+    
+    func getPreviousNode(of index:Int) -> Node<Value>? {
+        var node = head
+        var currentIndex = 0
+        while node?.next != nil, currentIndex < index-1 {
+            node = node?.next
+            currentIndex += 1
+        }
+        return node
+    }
+    
+    mutating func insert(_ value:Value, at index:Int) {
+        if index < 0 {
+            print("invalid index")
+        } else if index == 0 {
+            push(value: value)
+        } else {
+            let newNode = Node.init(value: value)
+            let targetNode = getPreviousNode(of: index)
+            
+            newNode.next = targetNode?.next
+            targetNode?.next = newNode
+        }
+    }
 }
 
 extension LinkedList: CustomStringConvertible {
@@ -42,7 +134,7 @@ extension Node: CustomStringConvertible {
         guard let next = next else {
             return "\(value)"
         }
-        return "\(value) ->  \(String.init(describing: next))"
+        return "\(value) -> \(next) " //recursive call
     }
 }
 
@@ -51,5 +143,20 @@ var list = LinkedList<Int>()
 list.push(value: 2)
 list.push(value: 3)
 list.push(value: 22)
+list.push(value: 33)
+list.append(value: 1)
 
+list.insert(5, at: 2)
+print(list)
+list.pop()
+print(list)
+list.removeLast()
+print(list)
+list.insert(5, at: 2)
+print(list)
+
+list.remove(at: 1)
+print(list)
+
+list.remove(at: 0)
 print(list)
